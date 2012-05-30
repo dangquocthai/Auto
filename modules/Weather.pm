b5a0c4b80adb4bde849de31bab75cbd012b8a462
Matthew <mattwb65@gmail.com>
2012-05-30 23:24:03 +0200

    Weather 1.0.1: Use Auto::HTTP

--- modules/Weather.pm
+++ modules/Weather.pm
@@ -1,12 +1,11 @@
 # Module: Weather. See below for documentation.
-# Copyright (C) 2010-2012 Xelhua Development Group, et al.
+# Copyright (C) 2010-2012 Arinity Development Group, et al.
 # This program is free software; rights to this code are stated in doc/LICENSE.
 package M::Weather;
 use strict;
 use warnings;
 use API::Std qw(cmd_add cmd_del trans);
 use API::IRC qw(privmsg notice);
-use LWP::UserAgent;
 use XML::Simple;
 
 # Initialization subroutine.
@@ -37,10 +36,6 @@ our %HELP_WEATHER = (
 sub weather {
     my ($src, @args) = @_;
 
-    # Create an instance of LWP::UserAgent.
-    my $ua = LWP::UserAgent->new();
-    $ua->agent('Auto IRC Bot');
-    $ua->timeout(2);
     # Put together the call to the Wunderground API. 
     if (!defined $args[0]) {
         notice($src->{svr}, $src->{nick}, trans('Not enough parameters').".");
@@ -50,34 +45,43 @@ sub weather {
     $loc =~ s/ /%20/g;
     my $url = "http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=".$loc;
     # Get the response via HTTP.
-    my $response = $ua->get($url);
+    $Auto::http->request(
+        url => $url,
+        on_response => sub {
+            my $response = shift;
 
-    if ($response->is_success) {
-    # If successful, decode the content.
-        my $d = XMLin($response->decoded_content);
-    # And send to channel
-        if (!ref($d->{observation_location}->{country})) {
-            my $windc = $d->{wind_string};
-            if (substr($windc, length($windc) - 1, 1) eq " ") { $windc = substr($windc, 0, length($windc) - 1) }
-            privmsg($src->{svr}, $src->{chan}, "Results for \2".$d->{observation_location}->{full}."\2 - \2Temperature:\2 ".$d->{temperature_string}." \2Wind Conditions:\2 ".$windc." \2Conditions:\2 ".$d->{weather});
-            privmsg($src->{svr}, $src->{chan}, "\2Heat index:\2 ".$d->{heat_index_string}." \2Humidity:\2 ".$d->{relative_humidity}." \2Pressure:\2 ".$d->{pressure_string}." - ".$d->{observation_time});
-        }
-        else {
-        # Otherwise, send an error message.
-            privmsg($src->{svr}, $src->{chan}, 'Location not found.');
-        }
-    }
-    else {
-    # Otherwise, send an error message.
-        privmsg($src->{svr}, $src->{chan}, 'An error occurred while retrieving your weather.');
-    }
+            if ($response->is_success) {
+                # If successful, decode the content.
+                my $d = XMLin($response->decoded_content);
+                # And send to channel
+                if (!ref($d->{observation_location}->{country})) {
+                    my $windc = $d->{wind_string};
+                    if (substr($windc, length($windc) - 1, 1) eq " ") { $windc = substr($windc, 0, length($windc) - 1) }
+                    privmsg($src->{svr}, $src->{target}, "Results for \2".$d->{observation_location}->{full}."\2 - \2Temperature:\2 ".$d->{temperature_string}." \2Wind Conditions:\2 ".$windc." \2Conditions:\2 ".$d->{weather});
+                    privmsg($src->{svr}, $src->{target}, "\2Heat index:\2 ".$d->{heat_index_string}." \2Humidity:\2 ".$d->{relative_humidity}." \2Pressure:\2 ".$d->{pressure_string}." - ".$d->{observation_time});
+                }
+                else {
+                    # Otherwise, send an error message.
+                    privmsg($src->{svr}, $src->{target}, 'Location not found.');
+                }
+            }
+            else {
+                # Otherwise, send an error message.
+                privmsg($src->{svr}, $src->{target}, 'An error occurred while retrieving your weather.');
+            }
+        },
+        on_error => sub {
+            my $error = shift;
+            privmsg($src->{svr}, $src->{target}, "An error occurred while retrieving your weather: $error");
+        },
+    );
 
     return 1;
 }
 
 # Start initialization.
-API::Std::mod_init('Weather', 'Xelhua', '1.00', '3.0.0a11');
-# build: cpan=LWP::UserAgent,XML::Simple perl=5.010000
+API::Std::mod_init('Weather', 'Arinity', '1.01', '3.0.0a11');
+# build: cpan=XML::Simple perl=5.010000
 
 __END__
 
@@ -115,10 +119,10 @@ From the NE at 9 MPH Gusting to 22 MPH Conditions: Overcast
 
 =over
 
-This module requires LWP::UserAgent and XML::Simple. Both are 
-obtainable from CPAN <http://www.cpan.org>.
+This module requires XML::Simple. It is obtainable from 
+CPAN <http://www.cpan.org>.
 
-This module is compatible with Auto version 3.0.0a10+.
+This module is compatible with Auto version 3.0.0a11+.
 
 =back
 

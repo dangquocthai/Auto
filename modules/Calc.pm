dfd34bd92d66ea568dd1b8e13fa9db973e64b954
Matthew <mattwb65@gmail.com>
2012-05-30 23:17:06 +0200

    Calc 1.02: Use Auto::HTTP

--- modules/Calc.pm
+++ modules/Calc.pm
@@ -1,12 +1,11 @@
 # Module: Calc. See below for documentation.
-# Copyright (C) 2010-2012 Xelhua Development Group, et al.
+# Copyright (C) 2010-2012 Arinity Development Group.
 # This program is free software; rights to this code are stated in doc/LICENSE.
 package M::Calc;
 use strict;
 use warnings;
 use API::Std qw(cmd_add cmd_del trans);
 use API::IRC qw(privmsg notice);
-use Furl;
 use URI::Escape;
 use JSON -support_by_pp;
 
@@ -38,12 +37,6 @@ our %FHELP_CALC = (
 sub cmd_calc {
     my ($src, @argv) = @_;
 
-    # Create an instance of Furl.
-    my $ua = Furl->new(
-        agent => 'Auto IRC Bot',
-        timeout => 5,
-    );
-
     # Create an instance of JSON.
     my $json = JSON->new();    
     
@@ -54,33 +47,39 @@ sub cmd_calc {
     }
     my $expr = join ' ', @argv;
     my $url = 'http://www.google.com/ig/calculator?q='.uri_escape($expr);
-    # Get the response via HTTP.
-    my $response = $ua->get($url);
+    $Auto::http->request(
+        url => $url,
+        on_response => sub {
+            my $response = shift;
+            if (!$response->is_success)
+            {
+                privmsg($src->{svr}, $src->{target}, 'An error occurred while sending your expression to Google Calculator.');
+                return;
+            }
+            # Get data + parse it.
+            my $data = $json->allow_nonref->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($response->decoded_content);
 
-    if ($response->is_success) {
-        # If successful, get the content.
-        my $data = $json->allow_nonref->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($response->content);
-
-        if ($data->{error} eq q{} or $data->{error} eq 0) {
-            # And send to channel
-            privmsg($src->{svr}, $src->{chan}, "Result: $data->{lhs} = $data->{rhs}");
+            if ($data->{error} eq q{} or $data->{error} eq 0) {
+                # And send to channel
+                privmsg($src->{svr}, $src->{target}, "Result: $data->{lhs} = $data->{rhs}");
+            }
+            else {
+                # Otherwise, send an error message.
+                privmsg($src->{svr}, $src->{target}, 'Google Calculator sent an error.');
+            }
+        },
+        on_error => sub {
+            my $error = shift;
+            privmsg($src->{svr}, $src->{target}, "An error occurred while sending your expression to Google Calculator: $error");
         }
-        else {
-            # Otherwise, send an error message.
-            privmsg($src->{svr}, $src->{chan}, 'Google Calculator sent an error.');
-        }
-    }
-    else {
-        # Otherwise, send an error message.
-        privmsg($src->{svr}, $src->{chan}, 'An error occurred while sending your expression to Google Calculator.');
-    }
+    );
 
     return 1;
 }
 
 # Start initialization.
-API::Std::mod_init('Calc', 'Xelhua', '1.01', '3.0.0a11');
-# build: cpan=Furl,URI::Escape,JSON,JSON::PP perl=5.010000
+API::Std::mod_init('Calc', 'Arinity', '1.02', '3.0.0a11');
+# build: cpan=URI::Escape,JSON,JSON::PP perl=5.010000
 
 __END__
 
@@ -90,7 +89,7 @@ Calc - Interface to Google Calculator
 
 =head1 VERSION
 
- 1.01
+ 1.02
 
 =head1 SYNOPSIS
 
@@ -108,10 +107,6 @@ This module depends on the following CPAN modules:
 
 =over
 
-=item L<Furl>
-
-This is the HTTP agent this module uses.
-
 =item L<URI::Escape>
 
 This is used to escape special characters from expressions.
@@ -126,11 +121,11 @@ This is used to parse the data returned by the Google Calculator API.
 
 This module was written by Matthew Barksdale.
 
-This module is maintained by Xelhua Development Group.
+This module is maintained by Arinity Development Group.
 
 =head1 LICENSE AND COPYRIGHT
 
-This module is Copyright 2010-2012 Xelhua Development Group.
+This module is Copyright 2010-2012 Arinity Development Group.
 
 Released under the same licensing terms as Auto itself.
 
